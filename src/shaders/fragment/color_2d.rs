89ec55d13671deabc253fc7b5d140508c801297b
Mike Burr <mb@unintuitive.org>
2021-07-28 08:27:19 -0600

    phase 1: make separate files for shader code

--- src/shaders/fragment/color_2d.rs
+++ src/shaders/fragment/color_2d.rs
@@ -1,10 +1,1 @@
-pub const SHADER: &str = r#"
-    precision mediump float;
-
-    uniform vec4 uColor;
-    uniform float uOpacity;
-
-    void main() {
-        gl_FragColor = vec4(uColor.r, uColor.g, uColor.b, uColor.a * uOpacity);
-    }
-"#;
+pub const SHADER: &str = include_str!("./color_2d.shader");
--- src/shaders/vertex/color_2d.rs
+++ src/shaders/vertex/color_2d.rs
@@ -1,8 +1,1 @@
-pub const SHADER: &str = r#"
-    attribute vec4 aPosition;
-    uniform mat4 uTransform;
-
-    void main() {
-        gl_Position = uTransform * aPosition;
-    }
-"#;
+pub const SHADER: &str = include_str!("./color_2d.shader");
